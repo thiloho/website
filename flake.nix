151f58d4ca3206c2493739ce5624dc0f7e327957
thiloho <123883702+thiloho@users.noreply.github.com>
2023-02-20 09:25:37 +0100

    Added flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "JavaScript development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs
+        ];
+      };
+    };
+}
